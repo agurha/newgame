d0d397094967f061ca10a78f3d90f4ab5ae2432e
cjroebuck <tophtfc@gmail.com>
2010-11-02 01:06:52 +0000

    added new Board model

---
+++ NoughtsAndCrossesMultiPlayer/Model/Board.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+
+namespace NoughtsAndCrossesMultiPlayer.Model
+{
+    /// <summary>
+    /// This class will model the Board, keeping track of status of each square in the grid
+    /// using a 3x3 array. A Board viewmodel will then wrap it and convert this to a single 
+    /// ObservableCollection of length 9...
+    /// </summary>
+    public class Board
+    {
+
+    }
+}
